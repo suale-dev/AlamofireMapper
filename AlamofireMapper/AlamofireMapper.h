ee0339fd737536563ea2ff42b66d7443b81000db
Sua Le <sua8051@gmail.com>
2018-08-21 12:17:43 +0700

    Initial Commit

---
+++ AlamofireMapper/AlamofireMapper.h
@@ -0,0 +1,19 @@
+//
+//  AlamofireMapper.h
+//  AlamofireMapper
+//
+//  Created by Sua Le on 8/21/18.
+//  Copyright © 2018 Sua Le. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlamofireMapper.
+FOUNDATION_EXPORT double AlamofireMapperVersionNumber;
+
+//! Project version string for AlamofireMapper.
+FOUNDATION_EXPORT const unsigned char AlamofireMapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlamofireMapper/PublicHeader.h>
+
+
